207a8c3333daf87d73d4f8ca2e737fcb0f079ba9
kon2raya24 <konturaya@gmail.com>
2026-06-25 13:40:36 +0000

    docs: add JSDoc comments to tosnakecase.ts

--- src/tosnakecase.ts
+++ src/tosnakecase.ts
@@ -1,3 +1,7 @@
+/**
+ * toSnakeCase
+ * @param str
+ */
 export function toSnakeCase(str: string): string {
   if (!str || typeof str !== "string") throw new Error("Input must be a non-empty string");
   return str.replace(/([A-Z])/g, "_$1").replace(/[-\s]+/g, "_").toLowerCase().replace(/^_/, "");
